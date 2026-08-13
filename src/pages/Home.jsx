72cd7c93976fea3656709537cd91ded94309acd8
Ana Laura de Araujo <anaraujo0847@gmail.com>
2026-08-12 23:58:17 -0300

    feat: rip dedinho

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,11 +31,6 @@ export default function Home() {
       )}
 
       <div ref={ref} className="shine-card relative rounded-[100%] px-12 py-8">
-        <img
-          src={comeCloser}
-          alt="Come closer"
-          className="absolute inset-0 z-10 m-auto max-h-64 max-w-64"
-        />
         <h1 className="relative z-20 text-5xl font-bold tracking-widest uppercase">
           Recaps
         </h1>
